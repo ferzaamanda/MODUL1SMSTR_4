8711a20d7c5e8c5490df6d70619974b54a94e19a
ferzaamanda <ferzaamanda3@gmail.com>
2023-03-06 20:24:01 +0700

    commit

---
+++ src/Hidangan.java
@@ -0,0 +1,16 @@
+public class Hidangan {
+    protected String namaHidangan;
+
+    public String getNamaHidanggan(){
+        return namaHidangan;
+    }
+    public void setNamaHidangan(String namaHidangan){
+        this.namaHidangan = namaHidangan;
+
+    }
+
+    public String disnatap() {
+        return "Makanan Dihidangkan";
+
+    }
+}
